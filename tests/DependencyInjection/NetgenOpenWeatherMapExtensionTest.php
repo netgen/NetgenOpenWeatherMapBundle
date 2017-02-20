0419521408b60ae5522938f138344a2d84a31761
Mario Blazek <mario.b@netgen.hr>
2017-02-20 10:05:52 +0100

    NetgenOpenWeatherMapExtension test

---
+++ tests/DependencyInjection/NetgenOpenWeatherMapExtensionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Netgen\Bundle\OpenWeatherMapBundle\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Netgen\Bundle\OpenWeatherMapBundle\DependencyInjection\NetgenOpenWeatherMapExtension;
+
+class NetgenOpenWeatherMapExtensionTest extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions()
+    {
+        return [
+            new NetgenOpenWeatherMapExtension(),
+        ];
+    }
+
+    public function testItSetsValidContainerParameters()
+    {
+        $this->container->setParameter('ezpublish.siteaccess.list', []);
+        $this->load();
+    }
+
+    protected function getMinimalConfiguration()
+    {
+        return [
+            'system' => [
+                'default' => [
+                    'api_settings' => [
+                        'api_key' => 'myApiKey',
+                        'units' => 'metric',
+                        'language' => 'en',
+                        'type' => 'accurate',
+                    ],
+                    'cache_settings' => [
+                        'handler' => 'memcached',
+                        'ttl' => 3600,
+                    ],
+                    'memcached_settings' => [
+                        'server' => 'localhost',
+                        'port' => 11211,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
